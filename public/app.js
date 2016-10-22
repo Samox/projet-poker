a76d8ab729f41bc97152835ccdc9e78c63c1e2d9
Sammy Teillet <sammy.teillet@gmail.com>
2016-10-22 14:22:02 +0200

    use router

--- public/app.js
+++ public/app.js
@@ -1,5 +1,26 @@
-var mainApp = angular.module('mainApp', ['ngMaterial']);
+var mainApp = angular.module('mainApp', ['ngMaterial', 'ngRoute']);
 console.log(mainApp);
 mainApp.controller('mainCtrl', function mainCtrl($scope) {
   console.log($scope);
 });
+
+mainApp.component('landing', {
+  templateUrl: 'templates/landing.html',
+  controller: function landingCtrl() {
+      this.showBoard = () => {
+        console.log('croute');
+      };
+    }
+})
+
+mainApp.config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/', {
+          template: '<landing></landing>'
+        }).
+        otherwise('/');
+    }
+  ]);
